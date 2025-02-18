7736e388acb77e3dff3d549e5937e319725abdbe
GarrixA <aphrogarrix10@gmail.com>
2025-02-18 14:37:30 +0200

    quick-ifx

--- src/routes/role.routes.ts
+++ src/routes/role.routes.ts
@@ -5,37 +5,37 @@ import roleController from "../controllers/roleController";
 const router = express.Router();
 
 router.post(
-	"/roles",
-	athenticate.authenticateUser,
-	athenticate.isAdmin,
-	roleController.createRole
+  "/roles",
+  athenticate.authenticateUser,
+  athenticate.isAdmin,
+  roleController.createRole
 );
 
 router.get(
-	"/roles",
-	athenticate.authenticateUser,
-	athenticate.isAdmin,
-	roleController.getRoles
+  "/roles",
+  athenticate.authenticateUser,
+  athenticate.isAdmin,
+  roleController.getRoles
 );
 
 router.get(
-	"/roles/:id",
-	athenticate.authenticateUser,
-	roleController.getRoleById
+  "/roles/:id",
+  athenticate.authenticateUser,
+  roleController.getRoleById
 );
 
 router.post(
-	"/users/:userId/roles",
-	athenticate.authenticateUser,
-	athenticate.isAdmin,
-	roleController.assignRole
+  "/users/:userId/roles",
+  // athenticate.authenticateUser,
+  // athenticate.isAdmin,
+  roleController.assignRole
 );
 
 router.delete(
-	"roles/:id",
-	athenticate.authenticateUser,
-	athenticate.isAdmin,
-	roleController.deleteRole
+  "roles/:id",
+  athenticate.authenticateUser,
+  athenticate.isAdmin,
+  roleController.deleteRole
 );
 
 export default router;
